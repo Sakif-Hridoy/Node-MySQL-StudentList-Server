55ee5052deb2e885c7b54887de8af6619f390120
Sakif <sakifworkspace@gmail.com>
2025-04-03 21:20:32 +0600

    update,delete

--- index.js
+++ index.js
@@ -36,6 +36,43 @@ app.post('/student',async(req,res)=>{
 })
 
 
+app.get('/read/:id',(req,res)=>{
+    const id = req.params.id;
+    const sql = `SELECT * FROM students WHERE id = ${id}`;
+    db.query(sql,(err,result)=>{
+        if(err) return res.json({Message:"Error inside database server"})
+            return res.json(result)
+    })
+})
+
+app.put('/update/:id', (req, res) => {
+    const sql = 'UPDATE students SET Name = ?, Email = ? WHERE ID = ?';
+    const id = req.params.id;
+    
+    console.log("Updating ID:", id);
+    console.log("Request Body:", req.body);
+
+    db.query(sql, [req.body.name, req.body.email, id], (err, result) => {
+        if (err) {
+            console.error("Database Update Error:", err);
+            return res.status(500).json({ message: "Error inside server", error: err });
+        }
+        return res.json({ message: "Update successful", result });
+    });
+});
+
+
+app.delete('/delete/:id',(req,res)=>{
+    const sql = "DELETE FROM students WHERE id=?";
+    const id = req.params.id;
+    db.query(sql,[id],(err,result)=>{
+        if(err) return res.json({Message:"Error inside database server"});
+        return res.json(result)
+    })
+})
+
+
+
 
 // app.get("/",async(req,res)=>{
 //     console.log("Hello Express")
